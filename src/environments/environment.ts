47648722ca474080f190f3302e682db982753923
Neil-Barot <neil.r.barot@gmail.com>
2024-04-03 04:09:08 -0500

    Made Environment Folder + Modified Services

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseURL: 'https://open-weather-map27.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host', 
+    XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com', 
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'd100f8f2f7msh97e3e1121bf54ecp129f59jsnd22640d05722',
+};
